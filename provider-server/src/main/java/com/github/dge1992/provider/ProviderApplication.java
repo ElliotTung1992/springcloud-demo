e18931e80f0f8869869d008296649912a4593dcc
dge1992 <dge_1992@163.com>
2019-07-22 17:59:11 +0800

    断路器监控

--- provider-server/src/main/java/com/github/dge1992/provider/ProviderApplication.java
+++ provider-server/src/main/java/com/github/dge1992/provider/ProviderApplication.java
@@ -1,9 +1,14 @@
 package com.github.dge1992.provider;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,7 +16,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableHystrix
+@EnableHystrixDashboard
 @RestController
+@EnableCircuitBreaker
 public class ProviderApplication {
 
     public static void main(String[] args) {
@@ -22,7 +30,12 @@ public class ProviderApplication {
     String port;
 
     @GetMapping("/hi")
+    @HystrixCommand(fallbackMethod = "hiError")
     public String home(@RequestParam(value = "name", defaultValue = "forezp") String name) {
         return "hi " + name + " ,i am from port:" + port;
+    }
+
+    public String hiError(String name) {
+        return "hi,"+name+",sorry,error!";
     }
 }
