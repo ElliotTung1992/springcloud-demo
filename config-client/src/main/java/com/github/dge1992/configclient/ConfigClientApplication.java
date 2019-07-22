7aab4bbdb02b6c598f4d18a3b734a1ee0fc8af25
dge1992 <dge_1992@163.com>
2019-07-22 11:16:18 +0800

    整合消息总线

--- config-client/src/main/java/com/github/dge1992/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/github/dge1992/configclient/ConfigClientApplication.java
@@ -3,11 +3,17 @@ package com.github.dge1992.configclient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 @RestController
+@RefreshScope
 public class ConfigClientApplication {
 
     public static void main(String[] args) {
