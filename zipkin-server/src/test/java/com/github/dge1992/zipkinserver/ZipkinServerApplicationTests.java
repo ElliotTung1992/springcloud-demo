1ff234881fb651eeb7f145faf7e7428be18cc1a1
dongbinyang <dge_1992@163.com>
2019-11-01 17:24:36 +0800

    整合链路

---
+++ zipkin-server/src/test/java/com/github/dge1992/zipkinserver/ZipkinServerApplicationTests.java
@@ -0,0 +1,12 @@
+package com.github.dge1992.zipkinserver;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipkinServerApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
