936bdee1918ad9f9c65eef5abc1fbd6d18e5a3d7
Elliot <dge_1992@163.com>
2024-03-14 06:12:03 +0800

    feature: 简单搭建

---
+++ eureka-center/src/test/java/com/elliot/eurekacenter/EurekaCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elliot.eurekacenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
--- order-center/src/main/java/com/elliot/ordercenter/OrderCenterApplication.java
+++ order-center/src/main/java/com/elliot/ordercenter/OrderCenterApplication.java
@@ -2,8 +2,10 @@ package com.elliot.ordercenter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class OrderCenterApplication {
 
     public static void main(String[] args) {
