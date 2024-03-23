c1ef348744bbb9910fb5a72788a12492ac69840b
Elliot <dge_1992@163.com>
2024-03-23 14:04:52 +0800

    feature: 基本搭建

---
+++ gateway-center/src/main/java/com/elliot/gatewaycenter/GatewayCenterApplication.java
@@ -0,0 +1,16 @@
+package com.elliot.gatewaycenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 网关服务
+ */
+@SpringBootApplication
+public class GatewayCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayCenterApplication.class, args);
+    }
+
+}
